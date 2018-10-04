3ebf7eaafb355ca76c001cc2c1ad4c4189b49d6e
Tito Zwane <drammatik@gmail.com>
2018-10-04 12:19:43 +0100

    fizzbuzz

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,38 @@
+
 $(() => {
 
-  const $feelingDiv = $('#feeling');
   const $buttons = $('button');
+  let $li = $('li');
 
-  // //looping through elements
-  //   $buttons.each((i, button) =>{
-  //     const $button = $(button);
-  //     //do somthing ...
-  //
-  //   });
-
-  console.log('Im here');;
-
-    //assigining event handlers to multiple elements
-  $buttons.on(/*nameOfEvent*/ 'click', /*callback*/(e) => {
-      const newFeeling = $(e.target).text();
-      $feelingDiv.text(newFeeling);
-      if(newFeeling === 'Sassy') {
-        $feelingDiv.css({ color: 'pink', fontFamily: 'cursive', fontSize: '40px' });
-       }
-       if(newFeeling === 'Silly') {
-         $feelingDiv.css({ color: 'orange', fontFamily: 'fantasy', fontSize: '32px' });
-       }
-       if(newFeeling === 'Sad') {
-         $feelingDiv.css({ color: 'grey', fontFamily: 'serif', fontSize: '36px' });
-       }
-       if(newFeeling === 'Submarine') {
-         $feelingDiv.css({ color: 'lightBlue', fontFamily: 'sans-serif', fontSize: '30px' });
-       }
+  $buttons.on('click',(e) => {
+      if ($buttons){
+        for (var i=1; i < 101; i++){
+           if (i % 15 === 0)
+            console.log("FizzBuzz");
+          else if (i % 3 === 0)
+            console.log("Fizz");
+          else if (i % 5 === 0)
+            console.log("Buzz");
+          else console.log(i);
+        }
+      }
     });
 
+    function fizz (num){
 
+      for (let i = 0; i <= num; i++){
+      let element = document.createElement('li')
+      element.innerText = "";
+      if(i % 3 === 0 ) {element.innerText += "Fizz";}
+      if(i % 5 === 0 ) {element.innerText += "Buzz";}
+      if(element.innerText === ""){element.innerText = i;}
 
+      document.getElementById('output').appendChild(element);
+
+       }
+
+    }
+
+    fizz(100)
 
 });
